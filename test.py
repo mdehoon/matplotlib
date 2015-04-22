185777135db7c4ac63f5522f7c23f72489da8c4b
Michiel de Hoon <mjldehoon@yahoo.com>
2015-04-22 20:59:29 +0900

    updating the test script

--- test.py
+++ test.py
@@ -1,13 +1,28 @@
 import sys
-import events_macosx
 import events
 import events_tkinter
+events_tkinter.add_timer = events.add_timer
+events_tkinter.remove_timer = events.remove_timer
+events_tkinter.create_socket = events.create_socket
+events_tkinter.delete_socket = events.delete_socket
+events_tkinter.wait_for_event = events.wait_for_event
 
 if sys.version_info[0] < 3:
     from Tkinter import *
 else:
     from tkinter import *
 
+def f(timer):
+    print 'hello1'
+    events.add_timer(1000, f)
+
+def g(timer):
+    print 'hello2'
+    events.add_timer(5000, g)
+
+f(None)
+g(None)
+
 t = Tk()
 t.eval('proc f {} { puts "hello"; after 1000 f }; f;')
-t.evalfile('widget')
+# t.evalfile('widget')
