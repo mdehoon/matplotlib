9eab6e7010276e0ee9e61a922c94a9db86f34883
Michiel de Hoon <mjldehoon@yahoo.com>
2015-05-02 01:16:41 +0900

    This test should work

--- test_tcltk_matplotlib.py
+++ test_tcltk_matplotlib.py
@@ -1,5 +1,5 @@
 import sys
-import Tkinter as Tk
+import Tkinter
 import events
 import events_tkinter
 events_tkinter.add_timer = events.add_timer
@@ -8,13 +8,10 @@ events_tkinter.create_socket = events.create_socket
 events_tkinter.delete_socket = events.delete_socket
 events_tkinter.wait_for_event = events.wait_for_event
 
+root = Tkinter.Tk()
+# b = Tkinter.Button(root, text="Hello, world!")
+# b.pack()
 
-window = Tk.Tk()
-window.withdraw()
-
-icon_fname = "lib/matplotlib/mpl-data/images/matplotlib.gif"
-print "Calling Tk.PhotoImage with file", icon_fname
-sys.stdout.flush()
-icon_img = Tk.PhotoImage(file=icon_fname)
-print "After calling Tk.PhotoImage"
-sys.stdout.flush()
+# import matplotlib
+# matplotlib.use("tkagg")
+# from pylab import *
